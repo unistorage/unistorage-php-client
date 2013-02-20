85d16877f2eca85bc241da25d48676bc668a08cc
m8rge <to.merge@gmail.com>
2013-02-20 14:37:11 +0600

    rename constants

--- Models/Files/ImageFile.php
+++ Models/Files/ImageFile.php
@@ -8,112 +8,112 @@ use Unistorage\Unistorage;
  */
 class ImageFile extends RegularFile
 {
-	/**
-	 * @var int
-	 */
-	protected $width;
+    /**
+     * @var int
+     */
+    protected $width;
 
-	/**
-	 * @var int
-	 */
-	protected $height;
+    /**
+     * @var int
+     */
+    protected $height;
 
-	const MODE_KEEP = 'keep';
-	const MODE_CROP = 'crop';
-	const MODE_RESIZE = 'resize';
+    const MODE_KEEP_RATIO = 'keep';
+    const MODE_CROP_RESIZE = 'crop';
+    const MODE_EXACT = 'resize';
 
-	const CORNER_LEFT_TOP = 'nw';
-	const CORNER_LEFT_BOTTOM = 'sw';
-	const CORNER_RIGHT_TOP = 'ne';
-	const CORNER_RIGHT_BOTTOM = 'se';
+    const CORNER_LEFT_TOP = 'nw';
+    const CORNER_LEFT_BOTTOM = 'sw';
+    const CORNER_RIGHT_TOP = 'ne';
+    const CORNER_RIGHT_BOTTOM = 'se';
 
-	public function getHeight()
-	{
-		return $this->height;
-	}
+    public function getHeight()
+    {
+        return $this->height;
+    }
 
-	public function getWidth()
-	{
-		return $this->width;
-	}
+    public function getWidth()
+    {
+        return $this->width;
+    }
 
-	/**
-	 * @param string $mode
-	 * @param int $width
-	 * @param int $height
-	 * @param Unistorage $unistorage
-	 * @return File
-	 */
-	public function resize($mode, $width, $height, $unistorage)
-	{
-		$actionParams = array(
-			'mode' => $mode,
-		);
-		if (!empty($width))
-			$actionParams['w'] = $width;
-		if (!empty($height))
-			$actionParams['h'] = $height;
+    /**
+     * @param string $mode
+     * @param int $width
+     * @param int $height
+     * @param Unistorage $unistorage
+     * @return File
+     */
+    public function resize($mode, $width, $height, $unistorage)
+    {
+        $actionParams = array(
+            'mode' => $mode,
+        );
+        if (!empty($width))
+            $actionParams['w'] = $width;
+        if (!empty($height))
+            $actionParams['h'] = $height;
 
-		return $unistorage->applyAction($this, RegularFile::ACTION_RESIZE, $actionParams);
-	}
+        return $unistorage->applyAction($this, RegularFile::ACTION_RESIZE, $actionParams);
+    }
 
-	/**
-	 * @param string $format
-	 * @param Unistorage $unistorage
-	 * @return File
-	 */
-	public function convert($format, $unistorage)
-	{
-		return $unistorage->applyAction($this, RegularFile::ACTION_CONVERT, array(
-			'to' => $format,
-		));
-	}
+    /**
+     * @param string $format
+     * @param Unistorage $unistorage
+     * @return File
+     */
+    public function convert($format, $unistorage)
+    {
+        return $unistorage->applyAction($this, RegularFile::ACTION_CONVERT, array(
+            'to' => $format,
+        ));
+    }
 
-	/**
-	 * @param Unistorage $unistorage
-	 * @return File
-	 */
-	public function grayscale($unistorage)
-	{
-		return $unistorage->applyAction($this, RegularFile::ACTION_GRAYSCALE);
-	}
+    /**
+     * @param Unistorage $unistorage
+     * @return File
+     */
+    public function grayscale($unistorage)
+    {
+        return $unistorage->applyAction($this, RegularFile::ACTION_GRAYSCALE);
+    }
 
-	/**
-	 * @param int $angle 90, 180, 270. CCW
-	 * @param Unistorage $unistorage
-	 * @return File
-	 */
-	public function rotate($angle, $unistorage)
-	{
-		return $unistorage->applyAction($this, RegularFile::ACTION_ROTATE, array(
-			'angle' => $angle,
-		));
-	}
+    /**
+     * @param int $angle 90, 180, 270. CCW
+     * @param Unistorage $unistorage
+     * @return File
+     */
+    public function rotate($angle, $unistorage)
+    {
+        return $unistorage->applyAction($this, RegularFile::ACTION_ROTATE, array(
+            'angle' => $angle,
+        ));
+    }
 
-	/**
-	 * $wmWidth, $wmHeight, $horizontalPadding, $verticalPadding my have following format:
-	 * <ul>
-	 * <li> (\d+)px - number calculates in pixels
-	 * <li> (\d+) - number calculates in percents
-	 * </ul>
-	 * @param ImageFile $watermark
-	 * @param string $wmWidth watermark width
-	 * @param string $wmHeight watermark height
-	 * @param string $horizontalPadding padding of watermark
-	 * @param string $verticalPadding padding of watermark
-	 * @param string $corner one of ImageFile::CORNER_*
-	 * @param Unistorage $unistorage
-	 * @return File
-	 */
-	public function watermark($watermark, $wmWidth, $wmHeight, $horizontalPadding, $verticalPadding, $corner, $unistorage)
-	{
-		return $unistorage->applyAction($this, RegularFile::ACTION_WATERMARK, array(
-			'watermark' => $watermark->resourceUri,
-			'w' => $wmWidth,
-			'h' => $wmHeight,
-			'w_pad' => $horizontalPadding,
-			'h_pad' => $verticalPadding,
-			'corner' => $corner,
-		));
-	}
+    /**
+     * $wmWidth, $wmHeight, $horizontalPadding, $verticalPadding my have following format:
+     * <ul>
+     * <li> (\d+)px - number calculates in pixels
+     * <li> (\d+) - number calculates in percents
+     * </ul>
+     * @param ImageFile $watermark
+     * @param string $wmWidth watermark width
+     * @param string $wmHeight watermark height
+     * @param string $horizontalPadding padding of watermark
+     * @param string $verticalPadding padding of watermark
+     * @param string $corner one of ImageFile::CORNER_*
+     * @param Unistorage $unistorage
+     * @return File
+     */
+    public function watermark($watermark, $wmWidth, $wmHeight, $horizontalPadding, $verticalPadding, $corner, $unistorage)
+    {
+        return $unistorage->applyAction($this, RegularFile::ACTION_WATERMARK, array(
+            'watermark' => $watermark->resourceUri,
+            'w' => $wmWidth,
+            'h' => $wmHeight,
+            'w_pad' => $horizontalPadding,
+            'h_pad' => $verticalPadding,
+            'corner' => $corner,
+        ));
+    }
 }
--- tests/unit/UploadTest.php
+++ tests/unit/UploadTest.php
@@ -10,94 +10,94 @@ use Unistorage\Models\Files\TemporaryFile;
 
 class UploadTest extends PHPUnit_Framework_TestCase
 {
-	/** @var Unistorage */
-	private $unistorage;
+    /** @var Unistorage */
+    private $unistorage;
 
-	protected function setUp()
-	{
-		if (is_null($this->unistorage)) {
-			$params = require(__DIR__.'/../params.php');
-			$this->unistorage = new Unistorage();
-			$this->unistorage->host = $params['host'];
-			$this->unistorage->token = $params['token'];
-		}
-	}
+    protected function setUp()
+    {
+        if (is_null($this->unistorage)) {
+            $params = require(__DIR__ . '/../params.php');
+            $this->unistorage = new Unistorage();
+            $this->unistorage->host = $params['host'];
+            $this->unistorage->token = $params['token'];
+        }
+    }
 
-	public function testImage()
-	{
-		/** @var $imageFile ImageFile */
-		$imageFile = $this->unistorage->uploadFile(FIXTURES_DIR.'/ImageFile.jpg');
-		$this->assertInstanceOf('Unistorage\Models\Files\ImageFile', $imageFile);
+    public function testImage()
+    {
+        /** @var $imageFile ImageFile */
+        $imageFile = $this->unistorage->uploadFile(FIXTURES_DIR . '/ImageFile.jpg');
+        $this->assertInstanceOf('Unistorage\Models\Files\ImageFile', $imageFile);
 
-		$resultFile = $imageFile->convert('png', $this->unistorage);
-		$this->assertTrue(
-			$resultFile instanceof ImageFile ||
-			$resultFile instanceof TemporaryFile ||
-			$resultFile instanceof PendingFile,
-			'Failed asserting type of resultFile after convert');
-		$resultFile = $imageFile->grayscale($this->unistorage);
-		$this->assertTrue(
-			$resultFile instanceof ImageFile ||
-			$resultFile instanceof TemporaryFile ||
-			$resultFile instanceof PendingFile,
-			'Failed asserting type of resultFile after grayscale');
-		$resultFile = $imageFile->resize(ImageFile::MODE_CROP, 50, 50, $this->unistorage);
-		$this->assertTrue(
-			$resultFile instanceof ImageFile ||
-			$resultFile instanceof TemporaryFile ||
-			$resultFile instanceof PendingFile,
-			'Failed asserting type of resultFile after resize');
-		$resultFile = $imageFile->rotate(90, $this->unistorage);
-		$this->assertTrue(
-			$resultFile instanceof ImageFile ||
-			$resultFile instanceof TemporaryFile ||
-			$resultFile instanceof PendingFile,
-			'Failed asserting type of resultFile after rotate');
-		$resultFile = $imageFile->watermark($imageFile, 20, 20, 20, 20, ImageFile::CORNER_RIGHT_BOTTOM, $this->unistorage);
-		$this->assertTrue(
-			$resultFile instanceof ImageFile ||
-			$resultFile instanceof TemporaryFile ||
-			$resultFile instanceof PendingFile,
-			'Failed asserting type of resultFile after watermark');
-	}
+        $resultFile = $imageFile->convert('png', $this->unistorage);
+        $this->assertTrue(
+            $resultFile instanceof ImageFile ||
+                $resultFile instanceof TemporaryFile ||
+                $resultFile instanceof PendingFile,
+            'Failed asserting type of resultFile after convert');
+        $resultFile = $imageFile->grayscale($this->unistorage);
+        $this->assertTrue(
+            $resultFile instanceof ImageFile ||
+                $resultFile instanceof TemporaryFile ||
+                $resultFile instanceof PendingFile,
+            'Failed asserting type of resultFile after grayscale');
+        $resultFile = $imageFile->resize(ImageFile::MODE_CROP_RESIZE, 50, 50, $this->unistorage);
+        $this->assertTrue(
+            $resultFile instanceof ImageFile ||
+                $resultFile instanceof TemporaryFile ||
+                $resultFile instanceof PendingFile,
+            'Failed asserting type of resultFile after resize');
+        $resultFile = $imageFile->rotate(90, $this->unistorage);
+        $this->assertTrue(
+            $resultFile instanceof ImageFile ||
+                $resultFile instanceof TemporaryFile ||
+                $resultFile instanceof PendingFile,
+            'Failed asserting type of resultFile after rotate');
+        $resultFile = $imageFile->watermark($imageFile, 20, 20, 20, 20, ImageFile::CORNER_RIGHT_BOTTOM, $this->unistorage);
+        $this->assertTrue(
+            $resultFile instanceof ImageFile ||
+                $resultFile instanceof TemporaryFile ||
+                $resultFile instanceof PendingFile,
+            'Failed asserting type of resultFile after watermark');
+    }
 
-	public function testTemplate()
-	{
-		$template = $this->unistorage->createTemplate(array(
-			RegularFile::ACTION_RESIZE => array(
-				'mode' => ImageFile::MODE_KEEP,
-				'w' => 50,
-				'h' => 50,
-			),
-			RegularFile::ACTION_GRAYSCALE => array(),
-		), RegularFile::FILE_TYPE_IMAGE);
-		$this->assertInstanceOf('Unistorage\Models\Template', $template);
+    public function testTemplate()
+    {
+        $template = $this->unistorage->createTemplate(array(
+            RegularFile::ACTION_RESIZE => array(
+                'mode' => ImageFile::MODE_KEEP_RATIO,
+                'w' => 50,
+                'h' => 50,
+            ),
+            RegularFile::ACTION_GRAYSCALE => array(),
+        ), RegularFile::FILE_TYPE_IMAGE);
+        $this->assertInstanceOf('Unistorage\Models\Template', $template);
 
-		/** @var $imageFile ImageFile */
-		$imageFile = $this->unistorage->uploadFile(FIXTURES_DIR.'/ImageFile.jpg');
-		$this->assertInstanceOf('Unistorage\Models\Files\ImageFile', $imageFile);
+        /** @var $imageFile ImageFile */
+        $imageFile = $this->unistorage->uploadFile(FIXTURES_DIR . '/ImageFile.jpg');
+        $this->assertInstanceOf('Unistorage\Models\Files\ImageFile', $imageFile);
 
-		$resultFile = $imageFile->apply($template, $this->unistorage);
-		$this->assertTrue(
-			$resultFile instanceof ImageFile ||
-				$resultFile instanceof TemporaryFile ||
-				$resultFile instanceof PendingFile,
-			'Failed asserting type of resultFile after apply template');
-	}
+        $resultFile = $imageFile->apply($template, $this->unistorage);
+        $this->assertTrue(
+            $resultFile instanceof ImageFile ||
+                $resultFile instanceof TemporaryFile ||
+                $resultFile instanceof PendingFile,
+            'Failed asserting type of resultFile after apply template');
+    }
 
-	public function testCreateZip()
-	{
-		$imageFile = $this->unistorage->uploadFile(FIXTURES_DIR.'/ImageFile.jpg');
-		$this->assertInstanceOf('Unistorage\Models\Files\ImageFile', $imageFile);
-		$zipFile = $this->unistorage->getZipped(array($imageFile), 'zipName.zip');
-		$this->assertInstanceOf('Unistorage\Models\Files\ZipFile', $zipFile);
-	}
+    public function testCreateZip()
+    {
+        $imageFile = $this->unistorage->uploadFile(FIXTURES_DIR . '/ImageFile.jpg');
+        $this->assertInstanceOf('Unistorage\Models\Files\ImageFile', $imageFile);
+        $zipFile = $this->unistorage->getZipped(array($imageFile), 'zipName.zip');
+        $this->assertInstanceOf('Unistorage\Models\Files\ZipFile', $zipFile);
+    }
 
-	public function testAudio()
-	{
-		/** @var $audioFile AudioFile */
-		$audioFile = $this->unistorage->uploadFile(FIXTURES_DIR.'/AudioFile.m4r');
-		$this->assertInstanceOf('Unistorage\Models\Files\AudioFile', $audioFile);
+    public function testAudio()
+    {
+        /** @var $audioFile AudioFile */
+        $audioFile = $this->unistorage->uploadFile(FIXTURES_DIR . '/AudioFile.m4r');
+        $this->assertInstanceOf('Unistorage\Models\Files\AudioFile', $audioFile);
 
         $this->assertNotEmpty($audioFile->bitrate);
         $this->assertNotEmpty($audioFile->duration);
@@ -112,18 +112,18 @@ class UploadTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($audioFile->unistorageType, 'audio');
     }
 
-	public function testVideo()
-	{
-		/** @var $videoFile VideoFile */
-		$videoFile = $this->unistorage->uploadFile(FIXTURES_DIR.'/VideoFile.mov');
-		$this->assertInstanceOf('Unistorage\Models\Files\VideoFile', $videoFile);
+    public function testVideo()
+    {
+        /** @var $videoFile VideoFile */
+        $videoFile = $this->unistorage->uploadFile(FIXTURES_DIR . '/VideoFile.mov');
+        $this->assertInstanceOf('Unistorage\Models\Files\VideoFile', $videoFile);
 
-		$resultFile = $videoFile->extractAudio('mp3', $this->unistorage);
-		$this->assertTrue(
-			$resultFile instanceof AudioFile ||
-				$resultFile instanceof TemporaryFile ||
-				$resultFile instanceof PendingFile,
-			'Failed asserting type of resultFile after extractAudio');
+        $resultFile = $videoFile->extractAudio('mp3', $this->unistorage);
+        $this->assertTrue(
+            $resultFile instanceof AudioFile ||
+                $resultFile instanceof TemporaryFile ||
+                $resultFile instanceof PendingFile,
+            'Failed asserting type of resultFile after extractAudio');
 
         $this->assertEquals($videoFile->size, 1516948);
         $this->assertEquals($videoFile->name, 'VideoFile.mov');
@@ -132,23 +132,23 @@ class UploadTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($videoFile->unistorageType, 'video');
         $this->assertNotEmpty($videoFile->url);
         $this->assertEquals($videoFile->videoWidth, 1280);
-		$this->assertNotEmpty($videoFile->audioBitrate);
-		$this->assertEquals($videoFile->audioCodec, 'aac');
-		$this->assertNotEmpty($videoFile->audioDuration);
-		$this->assertNotEmpty($videoFile->audioSampleRate);
-		$this->assertEquals($videoFile->audioChannels, 1);
-		$this->assertNotEmpty($videoFile->videoBitrate);
-		$this->assertEquals($videoFile->videoCodec, 'h264');
-		$this->assertNotEmpty($videoFile->videoDuration);
-		$this->assertEquals($videoFile->videoFps, 24);
-		$this->assertEquals($videoFile->videoHeight, 720);
-		$this->assertEquals($videoFile->videoWidth, 1280);
+        $this->assertNotEmpty($videoFile->audioBitrate);
+        $this->assertEquals($videoFile->audioCodec, 'aac');
+        $this->assertNotEmpty($videoFile->audioDuration);
+        $this->assertNotEmpty($videoFile->audioSampleRate);
+        $this->assertEquals($videoFile->audioChannels, 1);
+        $this->assertNotEmpty($videoFile->videoBitrate);
+        $this->assertEquals($videoFile->videoCodec, 'h264');
+        $this->assertNotEmpty($videoFile->videoDuration);
+        $this->assertEquals($videoFile->videoFps, 24);
+        $this->assertEquals($videoFile->videoHeight, 720);
+        $this->assertEquals($videoFile->videoWidth, 1280);
 
-		$resultFile = $videoFile->captureFrame('jpeg', 0, $this->unistorage);
-		$this->assertTrue(
-			$resultFile instanceof ImageFile ||
-				$resultFile instanceof TemporaryFile ||
-				$resultFile instanceof PendingFile,
-			'Failed asserting type of resultFile after captureFrame');
-	}
+        $resultFile = $videoFile->captureFrame('jpeg', 0, $this->unistorage);
+        $this->assertTrue(
+            $resultFile instanceof ImageFile ||
+                $resultFile instanceof TemporaryFile ||
+                $resultFile instanceof PendingFile,
+            'Failed asserting type of resultFile after captureFrame');
+    }
 }
